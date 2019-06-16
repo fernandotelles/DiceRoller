f76ec667d8eff3c756c70f5226d5f06b21f812b7
Fernando Teles <fernandotalves@gmail.com>
2019-06-16 14:04:09 -0300

    extract toast to rollDice method and add random numbers

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +16,14 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll!"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "You Clicked!", Toast.LENGTH_SHORT).show()
+            roolDice()
         }
+    }
+
+    private fun roolDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+        Toast.makeText(this, "You Clicked it ${resultText.text} times !", Toast.LENGTH_SHORT).show()
     }
 }
