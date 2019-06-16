73328abcef08a998b5a5d3210774043260260ead
Fernando Teles <fernandotalves@gmail.com>
2019-06-16 13:23:52 -0300

    Added an onclick listener to the rollbutton

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.android.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll!"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "You Clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
